cc1b57ccfe926f6d276b5841a8ab3b9e254b317d
Juan Miguel <88735626+juanmiguelosorioarroyave@users.noreply.github.com>
2022-04-05 15:15:15 -0500

    Cambio de className register

--- src/components/pages/Register/Resgiter.jsx
+++ src/components/pages/Register/Resgiter.jsx
@@ -1,12 +1,4 @@
-import React from 'react'
-import {
-    Combobox,
-    ComboboxInput,
-    ComboboxPopover,
-    ComboboxList,
-    ComboboxOption,
-    ComboboxOptionText,
-  } from "@reach/combobox";
+import React, { useState ,useEffect } from 'react'
 import logo from '../../../images/FMarket1.png'
 import logo_name from '../../../images/FMarket2.png'
 import Imag_Register from '../../../images/imgRegister.png'
@@ -14,58 +6,60 @@ import './Register.css'
 
 
 function Resgiter() {
+
+  const url = 'https://www.datos.gov.co/resource/xdk5-pm3f.json'
+  const [Depart , setDepart] = useState(0)
+  const fetchApi = async () => {
+    const responde = await fetch(url)
+    const responseDepart = await responde.json()
+    setDepart(responseDepart)
+  }
+  useEffect(() => {
+    fetchApi()
+
+  }, [] )
   return (
     <body>
-      <div className='Name-compani'>
-        <img className='logo' src={logo} alt='Logo' />
-        <img className='Name' src={logo_name} alt='Name'/>
+      <div className='Header-Resgiter'>
+        <img className='logo-Register' src={logo} alt='Logo' />
+        <img className='Name-Register' src={logo_name} alt='Name'/>
       </div>
       <div>
         <img className="Register" src={Imag_Register} alt='Login' /> 
         <div className="container-Resgiter">
-          <div className="img">
+          <div className="img-Register">
           </div>
           <div className="Register-content">
             <div className='formulari-Register'>
                 <h2 className="title-Register">REGISTRATE AQUI</h2>
-                <input type="text" placeholder='Nombre Completo'/>
-                <input type="text" placeholder='Alias' />
-                <input type="email" placeholder='Correo Electronico'/>
-                <input type="password" placeholder='Contraseña'/>    
-                <Combobox className='combobox-Register' >
-                    <ComboboxInput
-                        placeholder="Departamento"/>
-                    <ComboboxPopover>
-                        <ComboboxList>
-                        <ComboboxOption value='Quindio'>
-                            <ComboboxOptionText />
-                        </ComboboxOption>
-                </ComboboxList>
-                </ComboboxPopover>
-                </Combobox>
-                <Combobox >
-                    <ComboboxInput
-                        placeholder="Municipio"/>
-                    <ComboboxPopover>
-                        <ComboboxList>
-                        <ComboboxOption value='Montenegro'>
-                            <ComboboxOptionText />
-                        </ComboboxOption>
-                </ComboboxList>
-                </ComboboxPopover>
-                </Combobox>
+                <input type='text' placeholder='Nombre Completo'/>
+                <input type='text' placeholder='Alias' />
+                <input type='email' placeholder='Correo Electronico'/>
+                <input type='password' placeholder='Contraseña'/>    
+                <select>
+                  { !Depart ? 'Cargando...'  :
+                    Depart.map((Depart,index) => {
+                      return <option key={index} value="Seleccion su departamento...">{Depart.departamento} </option>
+                    })
+                  }
+                </select>
+                <select>
+                  { !Depart ? 'Cargando...' :
+                  Depart.map((Depart,index) => {
+                    return <option key={index} value='Seleccion su municipo...'>{Depart.municipio} </option>
+                  })}
+                </select> 
                  <input type='text' placeholder='Dirección' />
                  <input type='text' placeholder='Télefono' />
                 <a>Al hacer clic en "Regístrate" , Aceptas Nuestras Condiciones , la política de datos y la política de cookies. </a>
-                <button className="btn">Registrate</button>
+                <button className="btn-Register">Registrate</button>
                 <div className="hr" />
               </div>
-               <a href="foo">Ya tienes una cuenta</a>
+               <a>Ya tienes una cuenta</a>
           </div>
         </div>
       </div>
     </body>
   )
 }
-
 export default Resgiter
