23300b308f3bc88e9fc8d65ee76944c693223400
sebastianmiracastro <smira44@misena.edu.co>
2022-04-26 22:07:42 -0500

    Fixed Bug By Sebas [26042022]

--- src/components/pages/SingIn/SingIn.jsx
+++ src/components/pages/SingIn/SingIn.jsx
@@ -1,54 +1,33 @@
 import React, {useState} from 'react'
-import jwt from "jwt-decode";
 import './SingIn.css'
 import axios from 'axios';
 
-export const User=() => {
-
+const User=() => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  let URLPostLogin = 'https://fullmarket-provitional-backend.herokuapp.com/login'
-
-  var formData = new FormData();
+  var dataForm = new FormData();
 
   const HandleSubmit = async (e) => {
-    e.preventDefault()
-    formData.append("email", email)
-    formData.append("password", password)
+    dataForm.append('email', email)
+    dataForm.append('password', password)
 
-    axios.post(URLPostLogin).then(response => {
-      console.log(response);
-    }).catch(err => {
+    console.log(dataForm);
+    axios.post('https://fullmarket-provitional-backend.herokuapp.com/login', dataForm).then( (response => {
+      console.log(response.data);
+    })).catch( (err => {
       console.log(err);
-    })
-    };
-  return (
-  <form onSubmit={HandleSubmit} className="">  
-    <div className="login-content">
-      <div className='formulari'>
-        <h2 className="title">Inicar Sesion</h2> 
-        <input
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-        type="email" 
-        id='email' 
-        name='email' 
-        placeholder='Correo Electronico' />
-        <input 
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-        type="password" 
-        id='password' 
-        name='password' 
-        placeholder='password' 
-        autoComplete='off' />
-        <button  type='submit' className="btn">Iniciar Sesion </button>
-        <a href="foo" >Forgot Password?</a>
-        <div className="hr" /></div>
-     <button className="btn-new-acount">Crear cuenta nueva</button>
-    </div>
-  </form>
+    }))
+    e.preventDefault()
+    }
+
+    return (
+    <form onSubmit={HandleSubmit}>  
+      <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" placeholder='Correo Electronico'></input>
+      <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" placeholder='password'></input>
+      <button type='submit' >Iniciar Sesion </button>
+    </form>
   )
 }
-export default User
+
+export default User;
